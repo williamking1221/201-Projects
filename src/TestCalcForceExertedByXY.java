77b8bc3f73dd55a4807e726f78f64058cf097cfe
Daniel Hwang <daniel.hwang@duke.edu>
2022-01-16 23:04:21 +0000

    Update TestCalcForceExertedByXY.java - New F*dx/r and F/r*dx tests

--- src/TestCalcForceExertedByXY.java
+++ src/TestCalcForceExertedByXY.java
@@ -1,4 +1,3 @@
-
 import java.math.*;
 
 /**
@@ -44,5 +43,18 @@ public class TestCalcForceExertedByXY {
         checkEquals(p1.calcForceExertedByX(p3), 4.002e-11, "calcForceExertedByX()", 0.01);
         checkEquals(p1.calcForceExertedByY(p2), 0.0, "calcForceExertedByY()", 0.01);
         checkEquals(p1.calcForceExertedByY(p3), 5.336e-11, "calcForceExertedByY()", 0.01);
+
+        //Added Spring 2022
+        System.out.println("These tests check if you implemented these methods using F*dx/r and F*dy/r, not F/r*dx and F/r*dy");
+        System.out.println("If you fail these tests AND you've made the appropriate change above, it might be because of your Java runtime. Post a question on Ed.");
+        CelestialBody p4 =new CelestialBody(0.0, 0.0, 3.0, 4.0, 5*1e10, "jupiter.gif");
+        CelestialBody p5 =new CelestialBody(3000.0, 4000.0, 3.0, 4.0, 5*1e10 * Double.MIN_VALUE, "jupiter.gif");
+
+        //System.out.println(Double.MIN_VALUE);             4.9E-324
+        //System.out.println(p4.calcForceExertedBy(p5));    2.9644E-320
+
+        //Roundoff error causes next test to fail: Got 1.482E-320 and 1.9763E-320 respectively for F/r * dx and F/r * dy (which we don't want)
+        checkEquals(p4.calcForceExertedByX(p5), 1.7786E-320, "calcForceExertedByX()", 0.01);
+        checkEquals(p4.calcForceExertedByY(p5), 2.3715E-320, "calcForceExertedByY()", 0.01);
     }
 }
