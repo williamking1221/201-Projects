d997d915a1cf34c6ef5f26d8a5dc2022bce8467e
Brandon Fain <brandon.fain@duke.edu>
2022-01-18 16:16:53 +0000

    Updated TestCalcForceExertedByXY to comment buggy test

--- src/TestCalcForceExertedByXY.java
+++ src/TestCalcForceExertedByXY.java
@@ -45,6 +45,9 @@ public class TestCalcForceExertedByXY {
         checkEquals(p1.calcForceExertedByY(p3), 5.336e-11, "calcForceExertedByY()", 0.01);
 
         //Added Spring 2022
+        
+        /*
+        
         System.out.println("These tests check if you implemented these methods using F*dx/r and F*dy/r, not F/r*dx and F/r*dy");
         System.out.println("If you fail these tests AND you've made the appropriate change above, it might be because of your Java runtime. Post a question on Ed.");
         CelestialBody p4 =new CelestialBody(0.0, 0.0, 3.0, 4.0, 5*1e10, "jupiter.gif");
@@ -55,6 +58,7 @@ public class TestCalcForceExertedByXY {
 
         //Roundoff error causes next test to fail: Got 1.482E-320 and 1.9763E-320 respectively for F/r * dx and F/r * dy (which we don't want)
         checkEquals(p4.calcForceExertedByX(p5), 1.7786E-320, "calcForceExertedByX()", 0.01);
-        checkEquals(p4.calcForceExertedByY(p5), 2.3715E-320, "calcForceExertedByY()", 0.01);
+        checkEquals(p4.calcForceExertedByY(p5), 2.3715E-320, "calcForceExertedByY()", 0.01); 
+        */
     }
 }
